280b2c84dabcb8fc494561f3e4f24bbf3b902b63
mlekhi <maya.lekhi1@gmail.com>
2024-09-14 15:24:02 -0400

    new layout

--- client/src/pages/Interviewer.js
+++ client/src/pages/Interviewer.js
@@ -2,30 +2,36 @@
 import React from 'react';
 import Question from './Question';
 import Sandbox from './CodeSandbox';
+import Timer from '../components/Timer';
 
 const Interview = () => {
     return (
         <div className="min-h-screen text-white py-12 px-6 flex items-start pt-40 pb-40">
             <div className="container mx-auto flex flex-col lg:flex-row gap-12">
-                <div className="flex-1 lg:w-1/2">
-                    <h1 className="text-3xl font-bold mb-8 text-center lg:text-left">
-                        Problem
-                    </h1>
+                
+                <div className="lg:w-1/4">
                     <div className="mb-12 p-6 rounded-lg shadow-neon-blue">
                         <Question />
                     </div>
                 </div>
                 
-                <div className="flex-1 lg:w-1/2">
-                    <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
-                        Code Sandbox
-                    </h2>
+                <div className="lg:w-1/2">
                     <div className="p-6 rounded-lg shadow-neon-purple">
                         <Sandbox />
                     </div>
                 </div>
+
+                <div className="lg:w-1/4">
+                    <div className="mt-4 p-4 rounded-lg shadow-neon-blue">
+                        <Timer />
+                    </div>
+                    <div className="mt-4 p-4 rounded-lg">
+                        <img src="logo.png" alt="Logo" className="mx-auto max-w-full h-auto" />
+                    </div>
+                </div>
             </div>
         </div>
+
     );
 }
 
